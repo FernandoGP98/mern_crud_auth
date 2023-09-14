c2ad122658feaf0c6e3a39d9238886f66c08b20a
FernandoGomezPizana <fernando.gomez@netpay.com.mx>
2023-09-14 13:31:44 -0600

    Login y logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,14 +5,7 @@ import bcrypt from 'bcryptjs'
 import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
-    console.log(req.body)
     const {email, password, username} = req.body
-
-    // User.create({
-    //     username, 
-    //     email, 
-    //     password
-    // })
 
     try {
 
@@ -27,7 +20,7 @@ export const register = async (req, res) => {
             password: passHash,
             username
         })
-        console.log(newUser)
+
         // .save() guarda el este nuevo usuario
         //y como el metodo es asyncrono, hay que esperar a que termine.
         const userSaved = await newUser.save()
@@ -49,6 +42,46 @@ export const register = async (req, res) => {
 
 }
 
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        //Buscar el usuario si existe
+        const userFound = await User.findOne({email})
+
+        if(!userFound){
+            return res.status(400).json({message: "User not found"})
+        }
+        //Comparar la contraseña del usuario obtenido con la del payload
+        const passIsMatch = await bcrypt.compare(password, userFound.password) //regresa un boolean
+
+        if(!passIsMatch){
+            return res.status(400).json({message: "Incorrect password"})
+        }
+
+        //Generamos el token de usuario logeado
+        const token = await createAccessToken({id: userFound._id})
+
+        res.cookie("token", token)
+        //Lo siguiente es la respuesta que este servicio mandara al front
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        })
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+    
+
+}
+
+export const logout = (req, res) => {
+    //Obtenemos de la cookie con el nombre de token y su valor que creamos en cada registe y login
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    res.status(200).json({message: "User logout"})
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 const router = Router()
 
 //Peticiones con su ruta
 router.post('/register', register)
 router.post('/login', login)
+router.post('/logout', logout)
 
 //Se exporta el router porque las rutas deben añadir a la app de express
 export default router
